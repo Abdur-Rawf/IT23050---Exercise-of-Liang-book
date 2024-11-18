f8eeca5cc3564cac98349636d5366fddd8d30c50
Abdur-Rawf <rawf.ict20@gmail.com>
2024-11-18 10:23:06 +0600

    Add files via upload

---
+++ Ch1Ex2.java
@@ -0,0 +1,9 @@
+class Ch1Ex2{
+public static void main(String[] args){
+System.out.println("Welcome to Java");
+System.out.println("Welcome to Java");
+System.out.println("Welcome to Java");
+System.out.println("Welcome to Java");
+System.out.println("Welcome to Java");
+}
+}
